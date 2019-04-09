01a783a5d9a044356f8e62b6a18233b522b20aa0
paul-laird <35932444+paul-laird@users.noreply.github.com>
2019-04-09 19:20:19 +0100

    Update Form1.cs

--- HelloWorld/Form1.cs
+++ HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World");
+            MessageBox.Show("Hello to this class");
         }
 
         private void button2_Click(object sender, EventArgs e)
